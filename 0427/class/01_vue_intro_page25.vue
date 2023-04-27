b3b53ac26a4528b232e82492f4c3b04c3bcfe5bb
KwonMinWoo <kwmw0427@naver.com>
2023-04-27 16:41:12 +0900

    1

---
+++ 0427/class/01_vue_intro_page25.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- HTML -->
+  <div>
+    <p>Hello :)</p>
+  </div>
+</template>
+
+<script>
+  // JavaScript
+</script>
+
+<style>
+  /* CSS */
+  p {
+    color: black;
+  }
+</style>
+
+
+
+
